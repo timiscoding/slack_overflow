0ec0d06fb3dfab4116178f159ea7af8c835f5073
Timothy Tsui <timiscoding@gmail.com>
2015-12-14 16:36:25 +1100

    User name and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,9 @@
 #
 
 class User < ActiveRecord::Base
-  validates :name, :presence => true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => true, :length => { :minimum => 2, :maximum => 50 }
+  validates :email, :presence => true, :length => { :maximum => 255 }, :format => { :with => VALID_EMAIL_REGEX }, :uniqueness => { :case_sensitive => false }
   enum role: [:student, :teacher, :admin]
   has_many :posts
   has_many :comments
